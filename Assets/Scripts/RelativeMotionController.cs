376c24fbc3f0feccfd8cce3421984f255be5f3e2
Mouledoux <Mouledoux@users.noreply.github.com>
2017-11-11 17:07:16 -0600

    added basic sprite pack

--- Assets/Scripts/RelativeMotionController.cs
+++ Assets/Scripts/RelativeMotionController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Rigidbody))]
 public class RelativeMotionController : MonoBehaviour
 {
     private Vector3 m_localVelocity;
@@ -23,22 +24,44 @@ public class RelativeMotionController : MonoBehaviour
         }
     }
 
-    public bool m_useGravity;
-    [SerializeField]
-    private bool m_lockedInPlace;
+    [SerializeField] private float m_weight;
+    [SerializeField] private float m_drag;
+
+    [SerializeField] private float m_gravityMod;
+    [SerializeField] private bool m_lockedInPlace;
+
+    private Rigidbody m_rigidbody;
 
 	void Start ()
     {
-		
+        RigidbodySetUp();
 	}
 	
 	void Update ()
     {
-		
+        Move();
 	}
 
-    public void SetLocalVelocity(Vector3 newVel)
+    private int Move()
+    {
+        transform.Translate(m_localVelocity);
+
+        return 0;
+    }
+
+    private int RigidbodySetUp()
+    {
+        m_rigidbody = GetComponent<Rigidbody>();
+        if (m_rigidbody == null) m_rigidbody = gameObject.AddComponent<Rigidbody>();
+        m_rigidbody.isKinematic = true;
+
+        return 0;
+    }
+
+    private void SetLocalVelocity(Vector3 newVel)
     {
         m_localVelocity = newVel;
     }
+
+    
 }
